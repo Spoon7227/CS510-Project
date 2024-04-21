4fa4f9cb8626333a89e3fa03b789842f87830a92
josephjang <jyj3@illinois.edu>
2024-04-21 16:42:55 -0500

    fetchReponse Hook

--- frontend/src/components/Section.js
+++ frontend/src/components/Section.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import './styles/Section.css'
 
-function Section({ transcript, notes, onTranscriptChange, onNotesChange }) {
+function Section({ transcript, notes, setTranscriptContent, setNotesContent }) {
+    const onTranscriptChange = (event) => {
+        setTranscriptContent(event.target.value);
+    };
+
+    const onNotesChange = (event) => {
+        setNotesContent(event.target.value);
+    };
 
     return (
     <div className="section">
