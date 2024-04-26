d87fbdd926d95076e6a226298483c237ef4ede6e
Sudarshni-Ramesh <shruthiramesh2001@gmail.com>
2024-04-25 19:50:05 -0500

    adding styles

--- frontend/src/components/Section.js
+++ frontend/src/components/Section.js
@@ -13,24 +13,23 @@ function Section({ transcript, notes, setTranscriptContent, setNotesContent }) {
     return (
     <div className="section">
         <div className="subsection">
-        <h2>Transcript</h2>
-        <textarea
-            className="transcript"
-            value={transcript}
-            onChange={onTranscriptChange}
-            placeholder="Transcript"
-        />
+            <h2>Transcript</h2> 
+            <textarea
+                className="transcript"
+                value={transcript}
+                onChange={onTranscriptChange}
+                placeholder="Transcript"
+            />
         </div>
         <div className="subsection">
-            <h2>Notes</h2>
+            <h2>Notes</h2> 
             <textarea
                 className="notes"
                 value={notes}
                 onChange={onNotesChange}
                 placeholder="Notes"
-            />
+            />    
         </div>
-        
     </div>
     );
 }
